5a6e1dd528a04ac97bc1082c49928f1c9c418fc3
Krinitsky <krinitsky@sail.msk.ru>
2024-09-20 15:57:03 +0300

    cleanup

--- bot/config.py
+++ bot/config.py
@@ -14,10 +14,5 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 # config parameters
 telegram_token = config_yaml["telegram_token"]
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
-n_transcription_langs_per_page = config_yaml.get("n_transcription_langs_per_page", 6)
 mongodb_uri = f"mongodb://{config_env['MONGODB_HOSTNAME']}:{config_env['MONGODB_PORT']}"
 # telegram_base_url = config_yaml["telegram_base_url"]
-
-# transcription_langs
-with open(config_dir / "transcription_languages.yml", 'r') as f:
-    transcription_langs = yaml.safe_load(f)
